9cc93afa5ec94bd0841c9f0651af7b054d5851a2
Marius <mariusgutschalk@gmail.com>
2025-02-20 14:48:33 +0100

    added calorie.html & profile.html (not connected yet) calorie intake calculator is for now on the index page

--- server/server.js
+++ server/server.js
@@ -47,6 +47,9 @@ app.get("/", (request, response) => {
 app.get("/login", (request, response) => {
     response.render("login.html");
 });
+app.get("/calories.html", (request, response) => {
+    response.render("calorie.html");
+});
 app.get("/register", (request, response) => {
     response.render("register.html");
 });
@@ -56,6 +59,9 @@ app.post("/", (request, response) => {
     response.render("404.html");
 });
 app.get("/login", (request, response) => {
+    response.render("404.html");
+});
+app.get("/calorie", (request, response) => {
     response.render("404.html");
 });
 app.post("/register", (request, response) => {
