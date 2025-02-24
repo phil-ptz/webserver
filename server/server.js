6a4d0555b7b1fa7a9fe76a9cb7fb4fa494eacc7c
Phil Potratz <potratzphill@gmail.com>
2025-02-24 19:38:36 +0100

    server.js dokumentation

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(session({
     secret: "1234",
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false , maxAge: 30*60} // Cookie wird nicht verschlüsselt und hat eine Dauer von 30 Minuten
+    cookie: { secure: false , maxAge: 30*60*1000} // Cookie wird nicht verschlüsselt und hat eine Dauer von 30 Minuten
 }));
 
 // Für das Parsen der POST-Daten
@@ -40,41 +40,48 @@ const dbPath = "database/user_information.json"; // Pfad zur User-Datenbank
 // User zur Datenbank hinzufügen
 function pushUser (user) {
 
+    // Datenbank auslesen
     fs.readFile(dbPath, "utf8", (error, data) => {
+        // Error-Handling
         if (error) {
-            console.log("Fehler beim laden der Datenbank.");
-            console.log(error);
             return error;
         }
+
+        // Daten parsen
         dataObject = JSON.parse(data);
         userObject = JSON.parse(user);
+
+        // User hinzufügen ohne das conifirmed Password
         delete userObject["confirm_password"]
         dataObject.push(userObject);
+
+        // Datenbank wieder speichern
         fs.writeFile(dbPath, JSON.stringify(dataObject), (error) => {
-        if (error) {
-            console.log("Fehler beim Speichern der Daten.");
-            console.log(error);
-            return error;
-        } else {
-            console.log("Daten erfolgreich gespeichert.");
-        }
+            // Error-Handling
+            if (error) {
+                return error;
+            }
+        });
     });
-    })
+
+    // Kein Error => Neue Daten erfolgreich gespeichert
 }
 
 // Prüfen ob User in der Datenbank ist und gibt den User zurück
 function getUser (user, callback) {
 
+    // Datenbank auslesen
     fs.readFile(dbPath, "utf8", (error, data) => {
+        // Error-Handling
         if (error) {
-            console.log("Fehler beim laden der Datenbank.");
-            console.log(error);
             return callback(error);
         }
 
+        // Daten paresen
         dataObject = JSON.parse(data);
         userObject = JSON.parse(user);
 
+        // Suche nach Benutznamen und Passwort in Datenbank
         let found = null;
         for (const savedUser of dataObject) {
             if (savedUser["username"] == userObject["username"] && savedUser["password"] == userObject["password"]) {
@@ -82,46 +89,76 @@ function getUser (user, callback) {
             }
         }
 
+        // Gefunden Benutzernamen zurückgeben oder null wenn nicht gefunden
+        // Als Callback, um das Programm nicht zu blockieren
         callback(null, found);
     });
-
 }
 
-// Daten in der Datenbank speichern
+// Daten aus Rechnern in der Datenbank speichern
 function saveData(body, request) {
 
+    // Benutzername aus Session holen
     var username = request.session.user.username;
   
+    // Datenbank auslesen
     fs.readFile(dbPath, "utf8", (error, data) => {
+        // Error-Handling
         if (error) {
-            console.log("Fehler beim laden der Datenbank.");
-            console.log(error);
             return error;
         }
+
+        // Daten parsen
         dataObject = JSON.parse(data);
         bodyObject = JSON.parse(body);
 
+        // Nach User in Datenbank suchen
         for (var user of dataObject) {
             if (user["username"] == username) {
-                var merged = {...user, ...bodyObject}
 
+                // Daten zur Datenbank hinzufügen
+                var merged = {...user, ...bodyObject}
                 const index = dataObject.findIndex(user => user.username === username);
                 dataObject[index] = merged;
 
+                // Datenbank speichern
                 fs.writeFile(dbPath, JSON.stringify(dataObject), (error) => {
                     if (error) {
-                        console.log("Fehler beim Speichern der Daten.");
-                        console.log(error);
                         return error;
-                    } else {
-                        console.log("Daten erfolgreich gespeichert.");
                     }
                 });
             }
         }
     });
+
+    // Kein Error => Daten wurden erfolgreich gespeichert
 }
 
+// User aus der Datenbank löschen
+function deleteUser(username) {
+
+    // Datenbank laden
+    fs.readFile(dbPath, "utf8", (error, data) => {
+        if (error) {
+            return error;
+        }
+        
+        // Daten parsen
+        let dataObject = JSON.parse(data);
+
+        // User aus der Liste filtern
+        dataObject = dataObject.filter(user => user.username !== username);
+
+        // Aktualisierte Datenbank speichern
+        fs.writeFile(dbPath, JSON.stringify(dataObject), (error) => {
+            if (error) {
+                return error;
+            }
+        });
+    });
+
+    // Kein Error => Account wurde gelöscht
+}
 
 
 
@@ -129,6 +166,7 @@ function saveData(body, request) {
 
 // GET-Routen
 app.get("/", (request, response) => {
+    // Leite Benutzer zur Menu.html wenn wer angemeldet ist
     if (!request.session.user) {
         response.render("index.html");
     } else {
@@ -146,6 +184,7 @@ app.get("/impressum", (request, response) => {
 });
 
 // Geschützte Seiten, die nur nach Anmeldung aufgerufen werden können
+// Es wird nach aktiven Session geprüft
 app.get("/calculator", (request, response) => {
     if (!request.session.user) {
         return response.status(403).render("denied.html");
@@ -190,38 +229,22 @@ app.get("/delete", (request, response) => {
         return response.status(401).send("Nicht eingeloggt.");
     }
 
+    // Benutzername aus Session holen
     var username = request.session.user.username;
 
-    fs.readFile(dbPath, "utf8", (error, data) => {
+    // User löschen
+    var error = deleteUser(username);
+    if (error) {
+        response.status(500).send("Es konnte nicht auf die Datenbank zugegriffen werden.");
+    }
+
+    // Session löschen
+    request.session.destroy((error) => {
         if (error) {
-            console.log("Fehler beim Laden der Datenbank.");
-            console.log(error);
-            return response.status(500).send("Fehler beim Laden der Datenbank.");
+            response.status(500).send("Fehler beim löschen der Session.");
+        } else {
+            response.status(200).send(); // Weiterleitung erfolgt im Client-Script
         }
-        
-        let dataObject = JSON.parse(data);
-
-        // Nutzer aus der Liste filtern
-        dataObject = dataObject.filter(user => user.username !== username);
-
-        // Aktualisierte Datenbank speichern
-        fs.writeFile(dbPath, JSON.stringify(dataObject), (error) => {
-            if (error) {
-                console.log("Fehler beim Speichern der Daten.");
-                console.log(error);
-                return response.status(500).send("Fehler beim Speichern der Daten.");
-            }
-
-            console.log("Account gelöscht und Daten gespeichert.");
-
-            // Session löschen und Weiterleitung
-            request.session.destroy((err) => {
-                if (err) {
-                    return response.status(500).send("Fehler beim Logout.");
-                }
-                response.send('<script>alert("Ihr Account wurde gelöscht."); window.location.href="/login";</script>');
-            });
-        });
     });
 });
 // Prüfen ob der User eingeloggt ist + Benutzername geben
@@ -232,19 +255,23 @@ app.get("/check-login", (request, response) => {
         response.json({ loggedIn: false });
     }
 });
-// Profildaten bekommen
+// Profildaten bekommen (für profile.html)
 app.get("/get-profile", (request, response) => {
+    // Datenbank auslesen
     fs.readFile(dbPath, "utf8", (error, data) => {
         if (error) {
             return response.status(500).send("Fehler beim Laden der Datenbank.");
         }
+        // Nicht angemeldet
         if (!request.session.user) {
             return response.status(403).send("Nicht Authorisiert.");
         }
+        // Daten parsen und Benutzer herausfiltern
         dataObject = JSON.parse(data);
         username = request.session.user.username;
         const index = dataObject.findIndex(user => user.username === username);
         
+        // Daten in Response als json
         response.json(dataObject[index]);
     });
 });
@@ -254,28 +281,41 @@ app.get("/get-profile", (request, response) => {
 // POST-Routen
 // Login => ruft getUser() auf
 app.post("/login", (request, response) => {
+
+    // Request Body stringifizieren
     var body = JSON.stringify(request.body);
     getUser(body, (error, user) => {
         if (error == null) {
             if (user) {
-                // user in session speichern
+                // User in session speichern
                 request.session.user = { username: user.username };
-                //response.render("index.html")
+                // Weiterleitung zu /menu mit alert 
                 response.send('<script>alert("Erfolgreich eingeloggt."); window.location.href="/menu";</script>');
             } else {
+                // Fehler wenn falsche Zugangsdaten eingegeben
                 response.status(401).send('<script>alert("Falsche Zugangsdaten."); window.location.href="/login";</script>');
             }
         } else {
+            // Datenbank Fehler
             return response.status(500).send("Fehler beim Verarbeiten der Anfrage.");
         }
     });
 });
 // Register => ruft pushUser() auf
 app.post("/register", (request, response) => {
+
+    // Request Body stringifizieren
    var body = JSON.stringify(request.body);
+
+   // User zur Datenbank hinzufügen
    var error = pushUser(body);
+
+   // Weiterleitung zu /login
    if (error == null) {
         response.send('<script>alert("Sie wurden registriert, bitte melden Sie sich an."); window.location.href="/login";</script>');
+   } else {
+        // Datenbank Fehler
+        return response.status(500).send("Fehler beim Verarbeiten der Anfrage.");
    }
 });
 // Rechner-Daten speichern
@@ -285,11 +325,10 @@ app.post("/calculator", (request, response) => {
 });
 
 
-// 404 Für alle anderen Routes
+// Error-Seite Für alle anderen Routes
 app.get('*', function(request, response){
     response.status(404).render('404.html');
 });
-// 404 Für alle anderen Routes
 app.post('*', function(request, response){
     response.status(404).render('404.html');
 });
