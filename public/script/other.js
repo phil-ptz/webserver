79838fd741e2e2ec76e149dff5ede462559a1073
Marius <mariusgutschalk@gmail.com>
2025-02-25 12:33:31 +0100

    remove cookie banner

--- public/script/other.js
+++
@@ -1,5 +0,0 @@
-/* Cookie-Banner */
-function acceptCookies() {
-    document.getElementById('cookie-banner').style.display = 'none';
-    document.cookie = "cookiesAccepted=true; max-age=" + 60 * 60 * 24 * 365 + "; path=/";
-  }
